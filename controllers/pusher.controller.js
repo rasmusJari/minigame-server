a39400e9605b1b5e1ac190432a9b37a98041c984
Clemens Lieber <>
2026-02-20 18:24:20 +0100

    adds more debugging logs to pusher auth

--- controllers/pusher.controller.js
+++ controllers/pusher.controller.js
@@ -4,17 +4,22 @@ const Player = require("../models/Player");
 const pusher = require("../config/pusher");
 
 exports.authenticate = async (req, res) => {
-    console.log("Pusher auth request:", req.body);
-    const { socket_id, channel_name, playerId } = req.body;
+    try {
+        console.log("Pusher auth request:", req.body);
+        const {socket_id, channel_name, playerId} = req.body;
 
-    const expected = `private-player.${playerId}`;
-    if (channel_name !== expected)
-        return res.status(403).send("Forbidden");
+        const expected = `private-player.${playerId}`;
+        if (channel_name !== expected)
+            return res.status(403).send("Forbidden");
 
-    const player = await Player.findOne({ playerId });
-    if (!player)
-        return res.status(403).send("Invalid player");
+        const player = await Player.findOne({playerId});
+        if (!player)
+            return res.status(403).send("Invalid player");
 
-    const auth = pusher.authorizeChannel(socket_id, channel_name);
-    res.send(auth);
+        const auth = pusher.authorizeChannel(socket_id, channel_name);
+        res.send(auth);
+    } catch (err){
+        console.error("Pusher auth error:", err);
+        res.status(500).send("Server error");
+    }
 };
