869f6e31f4a7695aaf1c14047630c0d9bddd2258
Clemens Lieber <>
2026-02-20 18:09:53 +0100

    adds pusher auth endpoint to config

--- config/pusher.js
+++ config/pusher.js
@@ -5,6 +5,7 @@ const pusher = new Pusher({
     key: process.env.PUSHER_KEY,
     secret: process.env.PUSHER_SECRET,
     cluster: process.env.PUSHER_CLUSTER,
+    authEndpoint: process.env.PUSHER_AUTH_ENDPOINT,
     useTLS: true
 });
 
